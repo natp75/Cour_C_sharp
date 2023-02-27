8478d9d9be06e51a11feb4366c5776ee02af82b3
natp75 <natp75@mail.ru>
2023-02-27 19:23:59 +0300

    ДЗ 1 з 2

---
+++ lesson_1/HW/HW_1_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+int a = Console.ReadLine();
+int b = Console.ReadLine();
+if a > b
+{
+    Console.WriteLine("max = {a}");
+}
+else
+{
+    Console.WriteLine("max = {b}");
+}
+
+
+
