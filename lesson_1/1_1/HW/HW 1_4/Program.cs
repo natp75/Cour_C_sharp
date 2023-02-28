10bb428c80a03e6d3d479aea4074e84034a689b9
natp75 <natp75@mail.ru>
2023-02-28 19:15:39 +0300

    ДЗ 1_4

---
+++ lesson_1/1_1/HW/HW 1_4/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Введите число a: ");
+int a = Convert.ToInt32 (Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32 (Console.ReadLine());
+Console.Write("Введите число x: ");
+int x = Convert.ToInt32 (Console.ReadLine());
+int max = 0;
+if ( x < a & a > b)
+{
+    max = a;
+}
+else if (x < b & b > a)
+{
+    max = b;
+}
+else
+{
+    max = x;
+}
+Console.WriteLine($"{max}");
+
+
+
+Console.WriteLine("Hello, World!");
