90e4796f8ebab477b7606246c3645ad0027cd017
natp75 <natp75@mail.ru>
2023-03-04 18:04:51 +0300

    ДЗ 2_13

---
+++ lesson_2/HW/HW 2_13/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int number = new Random().Next(1,10000000);
+Console.WriteLine(number);
+string str = number.ToString();
+if (str.Length < 3)
+{
+Console.WriteLine("третьей цифры нет");
+}
+else
+{
+   int a = (int)Char.GetNumericValue(str[2]);
+   Console.WriteLine($"{a}"); 
+}
