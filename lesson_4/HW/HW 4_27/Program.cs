43beb8b13e6e030cad85eaa2b0d4b980a79626db
natp75 <natp75@mail.ru>
2023-03-09 19:11:06 +0300

    ДЗ 4_27

---
+++ lesson_4/HW/HW 4_27/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+
+int Division(int number)
+{
+    if (number < 10)
+        return number;
+    int end = number % 10;
+    int nextNumber = number / 10;
+    return end + Division(nextNumber);
+}
+
+int myNumber = new Random().Next(1, 300);
+Console.WriteLine($"Число = {myNumber}");
+int result = Division(myNumber);
+Console.WriteLine($"Сумма чисел = {result}");
