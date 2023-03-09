1e09f956a4993722202da0d3337a7376a2a71dcd
natp75 <natp75@mail.ru>
2023-03-09 19:21:08 +0300

    ДЗ 4_29

---
+++ lesson_4/HW/HW 4_29/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+
+int[] CreateRandomArray(int N, int start, int end)
+{
+    int[] RandomArray = new int[N];
+    for (int i=0; i < N; i++)
+    {
+        RandomArray[i] = new Random().Next(start, end + 1);
+    }
+    return RandomArray;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int[] Array = CreateRandomArray(8, -9, 9);
+{
+ShowArray(Array);
+}
