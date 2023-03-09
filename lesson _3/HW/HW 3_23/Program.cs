cb4a3a16ef5c629e6bacf69bef152958adffd8af
natp75 <natp75@mail.ru>
2023-03-09 18:31:39 +0300

    ДЗ 3_23

---
+++ lesson _3/HW/HW 3_23/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int number = new Random().Next(1, 100);
+Console.WriteLine(number);
+for (int i = 1; i <= 101; ++i)
+{
+    Console.Write($"{i * i * i}, ");
+}
