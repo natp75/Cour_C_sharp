4e5fbb4c72644f5f45ea551963654f42e87720cc
natp75 <natp75@mail.ru>
2023-03-09 19:05:23 +0300

    ДЗ 4_25

---
+++ lesson_4/HW/HW 3_25/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+
+int numberA = new Random().Next(1, 10);
+Console.WriteLine($"Число А = {numberA}");
+int numberB = new Random().Next(1, 10);
+Console.WriteLine($"Число B = {numberB}");
+
+int number = 1;
+for (int i = 1; i <= numberB; ++i)
+{
+    number = number * numberA;
+}
+ Console.WriteLine($"{number}");
