ad459a8cf5d465433e5d1d680e57d68604463e9e
natp75 <natp75@mail.ru>
2023-03-17 20:10:20 +0300

    ДЗ 5_38

---
+++ lesson_5/HW/HW 5_38/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+double[] CreateRandomArray(int N, int start, int end)
+{
+    double[] RandomArray = new double[N];
+    for (int i=0; i < N; i++)
+    {
+        RandomArray[i] = new Random().Next(start, end + 1);
+    }
+    return RandomArray;
+}
+
+void ShowArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+double[] Array = CreateRandomArray(4, -6, 10);
+double min = Array[0];
+double max = Array[0];
+{
+    ShowArray(Array);
+        for (int i = 0; i < Array.Length; i++)
+        {
+            if (Array[i] < min)
+                min = Array[i];
+            if (max < Array[i])
+                max = Array[i];
+        }
+        Console.WriteLine($"Min = {min}");
+        Console.WriteLine($"Max = {max}");
+        Console.WriteLine($"Разность Max и Min = {max - min}");
+}
